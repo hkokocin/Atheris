23b93b5aa1df33d21ff261d8220c3553f7e3f393
Hendrik Kokocinski <hendrik.kokocinski@weltn24.de>
2017-03-17 20:31:25 +0100

    separated viper from androidkit

--- androidKit/src/main/java/com/github/hkokocin/atheris/android/content/SharedPreferences.kt
+++
@@ -1,7 +0,0 @@
-package com.github.hkokocin.atheris.android.content
-
-import android.content.SharedPreferences
-
-fun SharedPreferences.setLong(key: String, value: Long) {
-    edit().putLong(key, value).apply()
-}
