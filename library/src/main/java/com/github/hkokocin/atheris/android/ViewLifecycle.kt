1675dc80ae28acb1cd7613cc7c37887bbc8f2408
Hendrik Kokocinski <hendrik.kokocinski@weltn24.de>
2017-04-22 21:58:34 +0200

    make ViewLifecycle functions mutable

--- library/src/main/java/com/github/hkokocin/atheris/android/ViewLifecycle.kt
+++ library/src/main/java/com/github/hkokocin/atheris/android/ViewLifecycle.kt
@@ -1,7 +1,7 @@
 package com.github.hkokocin.atheris.android
 
 class ViewLifecycle {
-    val onAttachedToWindow: () -> Unit = {}
-    val onDetachedFromWindow: () -> Unit = {}
-    val onViewCreated: () -> Unit = {}
+    var onAttachedToWindow: () -> Unit = {}
+    var onDetachedFromWindow: () -> Unit = {}
+    var onViewCreated: () -> Unit = {}
 }
